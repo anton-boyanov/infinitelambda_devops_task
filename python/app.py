de4f04430b966f6b67776c2e92465afacfd15e50
anton-boyanov <hmmmmm75@yahoo.com>
2020-11-18 12:52:09 +0200

    - fixes

--- python/app.py
+++ python/app.py
@@ -13,16 +13,15 @@ password = client.get_parameter(
     WithDecryption=True
 )
 source = boto3.client('rds', region_name='eu-west-1')
-
+instances = source.describe_db_instances(DBInstanceIdentifier='aboyanov')
+rds_host = instances.get('DBInstances')[0].get('Endpoint').get('Address')
+print(rds_host)
 try:
-    instances = source.describe_db_instances(DBInstanceIdentifier='aboyanov')
-    rds_host = instances.get('DBInstances')[0].get('Endpoint').get('Address')
-    print(rds_host)
     connection = psycopg2.connect(
         database="postgres",
         user=username.get("Parameter").get("Value"),
         password=password.get("Parameter").get("Value"),
-        host=rds_host,
+        host="aboyanov.chnffzayndnb.eu-west-1.rds.amazonaws.com",
         port='5432'
     )
     cursor = connection.cursor()
